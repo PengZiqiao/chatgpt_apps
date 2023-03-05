35adb1edbc0d334cae2032946700b5f6281fd0bd
Peng Ziqiao <xfyx.student@sina.com>
2023-03-06 00:15:11 +0800

    Add files via upload
    增加了few-shot prompting功能

--- application.py
+++ application.py
@@ -21,17 +21,18 @@ def index():
 def send():
     # 从请求中获取JSON数据
     data = request.get_json()
-    
+
     # 获得回复，并返回到前端
     content = chat(
-        prompts=data['prompts'], 
-        temperature=data['temperature'], 
-        system_message=data['systemMessage']
-        )
-    
+        prompts=data['prompts'],
+        temperature=data['temperature'],
+        system_message=data['systemMessage'],
+        few_shot_prompting=data['fewShotPrompting']
+    )
+
     return content
 
 
 if __name__ == "__main__":
-    # app.debug=True
+    app.debug = True
     app.run(host='0.0.0.0', port=int(sys.argv[1]))
--- chat_api.py
+++ chat_api.py
@@ -2,6 +2,8 @@ import openai
 import os
 
 # 对prompts进行切片，保留最后几项以控制总字符串长度
+
+
 def slice_prompts(prompts):
     # 初始化变量，记录 prompts 列表中的消息内容的总长度、子列表的起始索引
     total_length = 0
@@ -14,10 +16,13 @@ def slice_prompts(prompts):
         # 如果 total_length 变量大于 3000，就跳出循环
         if total_length > 2000:
             break
-    # 返回 prompts 列表中从 start_index-1 到末尾的子列表作为结果
-    return prompts[start_index-1:]
+        start_index -= 1
+    # 返回 prompts 倒数start_index-1位
+    return prompts[start_index:]
 
 # 定义一个装饰器，用来处理prompts
+
+
 def handle_prompts(func):
     def wrapper(self, prompt):
         # 将用户输入作为消息字典添加到self.prompts列表中
@@ -43,6 +48,8 @@ def handle_prompts(func):
     return wrapper
 
 # 定义一个session类，有相对固定的system_massag、temperature，调用时接受一个prompt，调用后会拼接记录prompts（见handle_prompts）
+
+
 class SessionChat:
     def __init__(self):
         # 设置openai的api密钥，从环境变量中获取
@@ -60,7 +67,8 @@ class SessionChat:
     def __call__(self, prompt):
         # 调用openai的ChatCompletion接口，传入self.prompts作为消息列表
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                messages=[self.system_massage] + self.prompts,
+                                                messages=[
+                                                    self.system_massage] + self.prompts,
                                                 temperature=self.temperature)
 
         # 获取返回结果中的回复内容
@@ -77,24 +85,30 @@ class SessionChat:
         self.prompts = []
 
 # 定义一个Chat类，每次调用接收全量prompts，可自定义system_message、temperature传入
+
+
 class Chat:
     def __init__(self):
         openai.api_key = os.environ['OPENAI_API_KEY']
 
-    def __call__(self, prompts, temperature=0, system_message=None):
+    def __call__(self, prompts, temperature=0, system_message=None, few_shot_prompting=None):
+        # 如果有 few_shot_prompting 参数，就把它到 prompts 列表前面
+        if few_shot_prompting:
+            prompts = few_shot_prompting + prompts
+
         # 如果有 system_message 参数，就把它作为第一个消息添加到 prompts 列表中
         if system_message:
-            prompts = [{'role':'system', 'content':system_message}] + prompts
+            prompts = [{'role': 'system', 'content': system_message}] + prompts
 
         # 对prompts进行切片，控制字符长度
         prompts = slice_prompts(prompts)
-        
+
         # 使用 openai 的 ChatCompletion 方法
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=prompts,
             temperature=temperature
-            )
+        )
         # 从响应中获取第一个选择的消息的内容，并返回
         content = response['choices'][0]['message']['content']
         return content
